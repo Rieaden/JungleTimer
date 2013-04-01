6a3da51359425b3f77f4ea7808238379beb968d1
Guillaume <guillaume.gedeon@laposte.net>
2013-04-01 21:27:06 +0200

    resized images

--- src/Util/ButtonChampion.java
+++ src/Util/ButtonChampion.java
@@ -2,8 +2,10 @@ package Util;
 
 import ImageAccess.Images;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 public class ButtonChampion extends Button 
 {
@@ -12,7 +14,7 @@ public class ButtonChampion extends Button
 	public ButtonChampion(String NomChampion)
 	{
 		champion = NomChampion;
-		Image tempImage = new Image(Images.getLienImage(NomChampion), 70, 70, false, false);
+		Image tempImage = new Image(Images.getLienImage(NomChampion), 50, 50, false, false);
 		ImageView tempImageView = new ImageView(tempImage);
 		tempImageView.setY(70);
 		tempImageView.setX(70);
